e93bb3e98511f1d13612d63ca67d04cc4f392d56
VishalNadig <82140384+VishalNadig@users.noreply.github.com>
2023-06-16 21:41:36 -0700

    Update update_graphic_drivers.sh

--- update_graphic_drivers.sh
+++ update_graphic_drivers.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb
